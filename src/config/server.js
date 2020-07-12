dfa132dce0d30243f596b0890dba438cf145fe96
Alessandro Menezes <alessandroadm@live.com>
2020-07-12 14:58:00 -0300

    Feat(api) Building the basic structure

---
+++ src/config/server.js
@@ -0,0 +1,9 @@
+const port = 3003;
+const express = require('express');
+const bodyParser = require('body-parser');
+const server = express();
+
+server.use(bodyParser.urlencoded({ extended: true }));
+server.listen(port, () => {
+    console.log('Server running on port', port);
+});
