bb356c7985e4b07c2c95820c9040e65eda869886
Alessandro <alessandroadm@live.com>
2020-08-04 23:53:38 -0300

    feat(cors): update cors config

--- src/config/server.js
+++ src/config/server.js
@@ -2,9 +2,9 @@ const port = 3003;
 const express = require('express');
 const bodyParser = require('body-parser');
 const server = express();
-const allowCors = require('./cors');
+const allowCors = require('cors');
 
-server.use(allowCors);
+server.use(allowCors());
 server.use(bodyParser.urlencoded({ extended: true }));
 server.listen(port, () => {
     console.log('Server running on port', port);
